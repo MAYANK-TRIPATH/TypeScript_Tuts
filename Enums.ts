adc173d7a5f996b946f5071d372b05110cdb4f1d
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-05 15:45:02 +0530

    Enums TUT

---
+++ Enums.ts
@@ -0,0 +1,21 @@
+// Enums allows us to define a set of named constants. 
+
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW = "Seat",
+    
+}
+
+const hcSeat = SeatChoice.AISLE
+
+// Sometimes JS code for TS file is so much. to get rid we use "const".
+
+const enum SeatChoice1 {
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW = "Seat",
+    
+}
+
+const hcSeat1 = SeatChoice.AISLE
