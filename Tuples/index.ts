242f5c7ee510c004f9cd69abd652132512b9f9bd
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-05 14:42:30 +0530

    Tuples tut

---
+++ Tuples/index.ts
@@ -0,0 +1,10 @@
+// Tuples :- It is a fixed lengh of array in which each element have particular type.
+
+let user: [number, string] = [1, 'Mayank'];  // adding more elements will throw error.
+user.push(1);  
+
+
+// Another eg. 
+let tUser: [string, number, boolean]
+
+tUser = ['name', 123, true]
