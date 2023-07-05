fefdef39f6e2498ee7b9bb809cd92ca9967d2abe
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-07-05 13:53:23 +0530

    Laerning basics

---
+++ Basics/src/index.ts
@@ -0,0 +1,11 @@
+// data types
+let sales = 123_234_432;
+let course = "TypeScript";
+let is_published = true;
+
+// any data type :- Try to avoid any data type coz typescript is for defining type(No point of using TS).
+let level;
+
+function render(document: any) {
+    console.log(document);
+}
